4634f43e1c2cf56128d56dd8c52b7fa83b157e1f
David Sotto Mayor <ddsmmf@gmail.com>
2020-09-20 15:01:05 -0400

    hide and show

--- base/js/script.js
+++ base/js/script.js
@@ -1,17 +1,8 @@
 $(function() {
-  // jQuery goes here...
+  
+  //show and hide changes DISPLAY!
+  $('.green-box').hide(1000);
+  $('.green-box').show(2000);
+  
 
-  // Uncomment this line to fade out the red box on page load
-
-  for(var i=0; i < 10; i++){
-
-    // FadeIn and FadeOut changes DISPLAY
-    // FadeTo changes OPACITY
-
-    $(".red-box").fadeOut(2000);
-    $(".red-box").fadeIn(4000);
-    $('.green-box').fadeTo(2000, 0.5);
-    $('.green-box').fadeTo(2000, 1);
-    $('.blue-box').fadeToggle(2000);
-  }
 });

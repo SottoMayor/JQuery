2faff5e75727fc5ded925924038556e24a56411b
David Sotto Mayor <ddsmmf@gmail.com>
2020-09-20 15:26:05 -0400

    animate and techniques

--- base/js/script.js
+++ base/js/script.js
@@ -1,11 +1,9 @@
 $(function() {
-  
-  //slideUp and SlideDown changes DISPLAY!
-  $('.green-box').slideUp(1000);
-  $('.green-box').slideDown(2000);
 
-  $('.blue-box').slideToggle(3000);
-  $('.blue-box').slideToggle(2000);
+  // += or -= changes the position in relation the CSS
+  $('.red-box').animate({
+    'marginLeft': '+=200px',
 
+  }, 1500,'linear')
   
 });

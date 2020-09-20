ae250797bb8b4d043ed0d2b77aad6a78785c597f
David Sotto Mayor <ddsmmf@gmail.com>
2020-09-20 17:36:28 -0400

    Jquery for Css

--- base/js/script.js
+++ base/js/script.js
@@ -1,5 +1,16 @@
 $(function(){
 
-  $('.lightbox').delay(500).fadeIn();
+  //$('p').css('background-color','yellow');
+  $('#list').css('background-color','yellow');
+  $('input[type="text"]').css('background-color','teal')
+  $('h2, p, input[type="email"]').css('background-color','chartreuse')
+  //$('li:last').css('background-color','lightblue')
+  //$('li:even').css('background-color','lightblue')
+  $('li:odd').css('background-color','lightblue')
+
+  //$('input[type="text"]').css('background-color','teal') too!! $(input:radio), $(input:radio)....
+  $('input:text').css('background-color','lightblue')
+  $('input:selected').css('background-color','yellow');
+  
 
 });

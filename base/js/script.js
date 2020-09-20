44d23d44c10c9f85853a143f9d9bdf89257bb0e1
David Sotto Mayor <ddsmmf@gmail.com>
2020-09-20 15:51:26 -0400

    creat a lightbox

--- base/js/script.js
+++ base/js/script.js
@@ -1,9 +1,5 @@
 $(function(){
 
-  $(".red-box").fadeTo(1000, 0.4, function() {
-    $(".green-box").fadeTo(1000, 0, function() {
-      $('.blue-box').fadeTo(1000, 0);
-    });
-  });
+  $('.lightbox').delay(500).fadeIn();
 
 });

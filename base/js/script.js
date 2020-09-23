ede45d22f88faaab5793c991077d8fc9b027952c
David Sotto Mayor <ddsmmf@gmail.com>
2020-09-23 16:19:09 -0400

    Adding or Removing CSS Classes

--- base/js/script.js
+++ base/js/script.js
@@ -1,23 +1,15 @@
 $(function(){
-  
+
   $('.gallery').hide();
+  $('a').addClass('fancy-link');
+  $('p:first').addClass('large emphasize');
 
-  var redBox = $('.red-box');
-  console.log(redBox.css('width'));
-  console.log(redBox.width());
-
-  redBox.css('background-color', 'orange');
-  $('p').css('font-size','19px');
-  redBox.css('width', '+=20px');
-
-
-  var props = $('p').css(['font-size', 'line-height', 'color']);
-  console.log(props);
-
-  //redBox.css('user-select','none');
-
-  redBox.css('user-select', function(){
-    return 'none';
+  $('div').addClass(function(index, current_class){
+    if(current_class === 'dummy'){
+      return 'red-box';
+    }
   })
+
+  $('.dummy').removeClass('dummy').addClass('green-box');
 
 });

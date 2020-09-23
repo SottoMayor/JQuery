a440205a33cce4c9b7349afdc8d6a77a324960d7
David Sotto Mayor <ddsmmf@gmail.com>
2020-09-23 18:11:54 -0400

    hover, mouseEnter and mouseLeave

--- base/js/script.js
+++ base/js/script.js
@@ -4,11 +4,27 @@ $(function(){
     alert('button was hovered!')
   })
 
+  //hover(handlerIn, handlerOut)
   $('.green-box').hover(function(){
     $(this).text('I was Hovered!')
   }, function(){
     $(this).text('Hover-me!')
   });
 
+  var blueBlox = $(".blue-box");
+
+  blueBlox.mouseenter(function(){
+    $(this).stop().fadeTo(1000, 0.5);
+  });
+  blueBlox.mouseleave(function(){
+    $(this).stop().fadeTo(1000, 1);
+  })
+
+
+  blueBlox.hover(function(){
+    $(this).stop().fadeTo(1000, 0.5);
+  }, function(){
+    $(this).stop().fadeTo(1000, 1);
+  });
 
 });

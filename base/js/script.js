f3d4704d3fe1c0406e70051c3a229260ec041486
David Sotto Mayor <ddsmmf@gmail.com>
2020-09-28 15:27:06 -0400

    Delegated Events

--- base/js/script.js
+++ base/js/script.js
@@ -1,29 +1,24 @@
 $(function(){
 
- // .on('click', function() {...})
+  /*
+  $('p').click(function(){
+    $(this).slideUp();
+  });
 
- function logEvent(){
-  console.log('Mouse was clicked or key was pressed!');
- }
+  $('#content').append('<p> This is a dinamically added paragraph! </p>');
 
- $('html').on('click keydown', logEvent());
+   */
 
-  var images = [
-    'images/laptop-mobile_small.jpg',
-    'images/laptop-on-table_small.jpg',
-    'images/people-office-group-team_small.jpg'
-  ]
-  var i = 0;
-  var gallery = $('.gallery').find('img');
-  gallery.on('click', execute);
+  // Delegated Events!
 
-  function execute (){
-    i = (i + 1) % images.length
-    gallery.fadeOut(function(){
-      gallery.attr('src',images[i]).fadeIn();
-    })
-  }
- 
+  $('#content').on('click', 'p', function(){
+    $(this).slideUp();
+  });
 
+  $('#content').append('<p> This is a dinamically added paragraph! </p>');
+
+  $('body').on('click mouseleave', 'li', function(){
+    $(this).css('color', 'blue');
+  })
 
 });

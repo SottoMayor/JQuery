5c01e501723f5a4ecda50dfdfa9408d1c949f669
David Sotto Mayor <ddsmmf@gmail.com>
2020-09-30 11:42:46 -0400

    handling the submit event

--- base/js/script.js
+++ base/js/script.js
@@ -1,26 +1,15 @@
 $(function(){
 
-
-
-  $('input[type="checkbox"]').change(function(){
-      var isChecked = $(this).is(':checked');
-
-      if(isChecked){
-          $(this).add('label[for="cb"]').css('box-shadow', '0 0 6px green');
-      }else{
-          $(this).add('label[for="cb"]').css('box-shadow', '0 0 6px red')
-
-      }
-  })
-
-  /*$('#option').change(function(){
-      alert($(this).val() + ' is my option!')
-  })
-  */
-
-  $('#option').change(function(){
-      myOption = $(this).find(':selected').text();
-      alert(myOption)
-  })
+    $('form').submit(function(){
+        if($('textarea').val().trim() == ''){
+            $('textarea').css('box-shadow', '0 0 6px red')
+            $(this).find('#write').append('<p>input a mensage!<p/>').css({
+                fontSize: "10px",
+                fontWeight: 800,
+                color: 'red'
+            })
+            return false
+        }
+    })
 
 });

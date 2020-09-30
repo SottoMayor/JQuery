825db8818609448dffc86b87c7af4b670e5a8f08
David Sotto Mayor <ddsmmf@gmail.com>
2020-09-30 15:37:24 -0400

    form validate

--- base/js/script.js
+++ base/js/script.js
@@ -1,15 +1,58 @@
 $(function(){
 
-    $('form').submit(function(){
-        if($('textarea').val().trim() == ''){
-            $('textarea').css('box-shadow', '0 0 6px red')
-            $(this).find('#write').append('<p>input a mensage!<p/>').css({
-                fontSize: "10px",
-                fontWeight: 800,
-                color: 'red'
-            })
-            return false
+    $('#form').submit(function(){
+        var name = $('#name').val();
+        var password = $('#password').val();
+        var message = $('#message').val();
+        var checkbox = $('#checkbox').is(':checked');
+
+        nameValidation(name);
+        passwordValidation(password);
+        messageValidation(message);
+        checkedValidation(checkbox);
+
+        return false;
+    });
+
+    function nameValidation(name){
+        var feedback = $('#name-feedback');
+        if(name.length < 2){
+            feedback.text('Please enter your name!').css('color', 'red');
+            return false;
+        }else{
+            feedback.text('');
         }
-    })
+    }
+
+    function passwordValidation(password){
+        var feedback = $('#password-feedback');
+        if(password.length > 6 && /.*[0-9]*./){
+            feedback.text('');
+        }else{
+            feedback.text('The password should have at least 6 characters and 1 number!').css('color', 'red');
+            return false;
+        }
+    }
+
+    function messageValidation(message){
+        var feedback = $('#message-feedback');
+        if(message.trim() != ''){
+            feedback.text('');
+        }else{
+            feedback.text('You should write your message!').css('color', 'red');
+            return false;
+        }
+    }
+
+    function checkedValidation(checkbox){
+        var feedback = $('#checkbox-feedback');
+        if(checkbox){
+            feedback.text('');
+        }else{
+            feedback.text('you must accept the terms of use').css('color', 'red');
+            return false;
+        }
+    }
 
 });
+

48f8b2395adb1f1680b66eebb5aa6c2b8289ab60
David Sotto Mayor <ddsmmf@gmail.com>
2020-10-02 11:05:59 -0400

    child selectors

--- js/code.js
+++ js/code.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
-    
-    $('.main-header').parent().css('background-color', 'orange');
+    // All children
+    //$('.parent').children().css('background-color', 'blue');
 
-    $('.post').parent().css('background-color', 'green');
-
-    $('.child').parent().css('background-color','teal')
-
+    $('.parent').children('.span').css('background-color', 'blue');
+    $('.parent div:first-child').css('color', 'white');
+    $('.parent div:last-child').css('color', 'red');
+    $('.parent div:nth-of-type(2)').css('color', 'orange');
+    $('.parent div:nth-last-of-type(2)').css('color', 'gray');
+    $('.parent').find('.sub-child').css('background-color','green')
 })

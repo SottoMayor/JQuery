5cf4950177a0cbb931264e20a71d74e679ad0075
David Sotto Mayor <ddsmmf@gmail.com>
2020-10-02 11:16:13 -0400

    odd and even selectors

--- js/code.js
+++ js/code.js
@@ -1,11 +1,10 @@
 $(document).ready(function(){
-    // All children
-    //$('.parent').children().css('background-color', 'blue');
 
-    $('.parent').children('.span').css('background-color', 'blue');
-    $('.parent div:first-child').css('color', 'white');
-    $('.parent div:last-child').css('color', 'red');
-    $('.parent div:nth-of-type(2)').css('color', 'orange');
-    $('.parent div:nth-last-of-type(2)').css('color', 'gray');
-    $('.parent').find('.sub-child').css('background-color','green')
+    $('#content div:odd').css('background-color','blue');
+    $('#content div:even').css('background-color','red');
+
+    $('thead').css('background-color','lightgray');
+    $('tbody tr:odd').css('background-color','orange');
+    $('tbody tr:even').css('background-color','chartreuse');
+
 })

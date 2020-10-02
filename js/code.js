bb7f00607b5d633ee1b1d3cbaa7babd467500ade
David Sotto Mayor <ddsmmf@gmail.com>
2020-10-02 10:14:29 -0400

    select element by tag name

---
+++ js/code.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('header').css('font-size', '30px').css('text-align', 'center');
+    $('section').css('background-color', 'lightgray');
+    $('p').css('font-size', '13px');
+    $('h6').css('font-weight', '800');
+    $('article').css('color', 'blue');
+});

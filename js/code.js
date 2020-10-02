2ab7f7e1532be0fc7230686ba1b67a9cfad37a84
David Sotto Mayor <ddsmmf@gmail.com>
2020-10-02 17:59:22 -0400

    Inner/ Outer width/height

--- js/code.js
+++ js/code.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
 
-    $('.box-1').width('400px').css('background-color','blue');
-    $('.box-2').height('400px').css('background-color','gray');
-    $('.box-3').width('200px').height('200px').css('background-color','yellow')
+    // innerHeight or innerWidth include padding!!
+    console.log($('div').innerHeight())
+    console.log($('div').innerWidth())
 
+    // outerHeight or outerWidht include padding and border!
+    console.log($('div').outerHeight())
+    console.log($('div').outerWidth())
+    console.log($('div').outerWidth() - $('div').innerWidth())
 })

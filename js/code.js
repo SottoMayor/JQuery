80e6703c271ca4f5291cdbbf58a18b58f8d32e15
David Sotto Mayor <ddsmmf@gmail.com>
2020-10-02 18:21:21 -0400

    Scroll left

--- js/code.js
+++ js/code.js
@@ -1,21 +1,10 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-    var box = $('.box');
+    $('button').click(function(){
+        
+        console.log($('div').scrollLeft());
+    })
 
-    //Position on the document left and top of the element!
-    console.log(box.offset());
-
-    //Position on the document left of the element!
-    console.log(box.offset().left);
-
-     //Position on the document top of the element!
-     console.log(box.offset().top);
-
-     //alert('top: '+ box.offset().top +' and left: '+box.offset().left)
-
-     // Position on the parent in relation of the element (if position:relative!)!
-     alert(box.position().top + ' and ' + box.position().left)
-
-
+    
 
 })

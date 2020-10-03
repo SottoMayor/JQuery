d2f1d61096c17279345e4118ae03df75c1b832d1
David Sotto Mayor <ddsmmf@gmail.com>
2020-10-03 10:32:19 -0400

    wrapAll and wrapInner

--- js/code.js
+++ js/code.js
@@ -1,9 +1,19 @@
 $(document).ready(function(){
     //Insertion arround!
 
-    //$('p').wrap('<div class="wrap"></div>');
+    //this function move selected elements by an only element-wrap!
+    $('p').wrapAll('<div class="wrapAll"></div>');
 
-    $('p').wrap(function(){
-        return '<p class="wrap"></div>'
+    //This function wrap the content of the element selected!
+    $('p').wrapInner('<b></b>')
+
+    $('.wrapAll').css({
+        'width':'300px',
+        'height':'300px',
+        'background-color':'gray',
+        'display': 'flex',
+        'justify-content': 'center',
+        'align-items':'center'
     })
+
 })

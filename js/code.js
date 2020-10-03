b3688e8c69ec1d61998c7ab9210801d484d7c910
David Sotto Mayor <ddsmmf@gmail.com>
2020-10-03 17:47:25 -0400

    working with classes

--- js/code.js
+++ js/code.js
@@ -1,10 +1,14 @@
 $(document).ready(function(){
+   /*
+    if($('div').hasClass('my-class')){
+        $('div').removeClass('my-class');
+    }else{
+        $('div').addClass('my-class');
+    }
+   */ 
     
-    //insert before!
-    $('p').before('<h3>Hello World!</h3>'); // or $('<h3>Hello World!</h3>').insertBefore('p')
-
-    //insert after!
-    $('p').after('<h2>Good Morning!</h2>'); // or $('<h2>Good Morning!</h2>').insertAfter('p')
+    $('div').toggleClass('my-class');
+    
 
 
 })

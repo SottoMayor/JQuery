b91fe0a4dbb3eaee03d8bc9bc1a2db3373ed4b75
David Sotto Mayor <ddsmmf@gmail.com>
2020-10-03 18:26:42 -0400

    mouse events

--- js/code.js
+++ js/code.js
@@ -1,14 +1,29 @@
 $(document).ready(function(){
-   /*
-    if($('div').hasClass('my-class')){
-        $('div').removeClass('my-class');
-    }else{
-        $('div').addClass('my-class');
-    }
-   */ 
-    
-    $('div').toggleClass('my-class');
-    
+   
+    $('.container').click(function(){
+        alert("You've Clicked")
+    })
 
+    $('.container').hover(function(){
+        $(this).css("background-color",'blue');
+    }, function(){
+        $(this).css("background-color",'white');
+    });
+    
+    /*$('#content').mousedown(function(){
+        console.log('Mouse Down!')
+    })
 
+    $('#content').mouseup(function(){
+        console.log('Mouse Up!')
+    })
+    */
+
+    $('#content').click(function(){
+        console.log('mousedown and mouseup!')
+    })
+
+    $('#content').mousemove(function(){
+        console.log('Mouse Move!!')
+    })
 })

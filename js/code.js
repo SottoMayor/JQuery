8cc3170809f4cf4fb8a7a712e7e1ec6ba2261c2a
David Sotto Mayor <ddsmmf@gmail.com>
2020-10-05 19:31:38 -0400

    slideUp, slideDown, slideToggle

--- js/code.js
+++ js/code.js
@@ -2,26 +2,23 @@ $(document).ready(function() {
 
     var box = $('.box');
 
+
     /*
-    box.css('display','none');
-   $('.fadeIn').click(function(){
-       box.fadeIn(1000, function(){
-           console.log('Done!')
-       });
-   })
+    
+     $('.slideUp').click(function(){
+        box.slideUp(1000);
+    })
 
-   $('.fadeOut').click(function(){
-       box.fadeOut(1000, function(){
-           console.log('Done Again!')
-       })
-   })
-   */
+    $('.slideDown').click(function(){
+        box.slideDown(1000);
+    })
 
-   $('.fadeToggle').click(function(){
-       box.fadeToggle('slow', function(){
-           console.log('BORA!')
-       })
-   })
+    */
+
+    $('.slideToggle').click(function(){
+        box.slideToggle('slow');
+    })
+   
 
     
 });

6d25072c08f0cdd9e4076986ab60cbff1cfffadd
David Sotto Mayor <ddsmmf@gmail.com>
2020-10-06 11:09:38 -0400

    Ajax load

--- js/code.js
+++ js/code.js
@@ -1,34 +1,7 @@
-$(document).ready(function() {
+$(document).ready(function(){
 
-    var box = $('.box');
+    $('button').click(function(){
+        $('.container').load('demo.html');
+    })
 
-    /*
-    $(document).click(function(){
-        box.animate({
-            left: '10px',
-            top: "20px",
-            width: '50px',
-            height: '50px'
-        }, 5000, function(){
-            console.log('Animation Done!')
-        })
-    });
-    */
-
-   $('.box').mousemove(function(){
-    box.animate({
-        left: '10px',
-        top: "20px",
-        width: '50px',
-        height: '50px'
-    }, 5000, function(){
-        console.log('Animation Done!')
-        });
-
-    });
-
-    
-
-    
-});
-
+})

c63a83a2645c7fc433081225c0c551b4df209e43
David Sotto Mayor <ddsmmf@gmail.com>
2020-10-07 11:05:06 -0400

    ScrollTop

--- js/code.js
+++ js/code.js
@@ -1,13 +1,20 @@
 $(document).ready(function(){
 
-    $('input:text').keyup(function(){
-        var userName = $(this).val();
-        
-        $.post('demo.php', {
-            name: userName
-        }, function(data, status){
-            $('#text').html(data);
-        })
+    var scroll_top = $('.scroll-top');
+
+    $(window).scroll(function(){
+
+        if( $(this).scrollTop() > 250 ){
+            scroll_top.stop().fadeIn(700);
+        }else{
+            scroll_top.stop().fadeOut(700);
+        }
+
     })
 
+    scroll_top.click(function(){
+        $('html, body').animate({
+            scrollTop: 0
+        }, 750)
+    })
 })  
